284df9a87125c57e7a996ebaee92d2f99558b067
Jacob Philip <jacobauxilium@gmail.com>
2024-04-04 15:15:11 +0530

    file edited

--- src/number_recognition.py
+++ src/number_recognition.py
@@ -1,3 +1,5 @@
 # this module is used to identify the numbers written in each cell using the model generated
 def recognise(cells) -> dict[int, list[int]]:
     raise NotImplemented("recognise() function is yet to be implemented")
+
+# this file is edited

fa608a6d199710f68a76f99ffe5dc9e5da3d5b93
don-sabu <donsabu@tutanota.com>
2024-04-06 01:25:53 +0530

    Added function to read image

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src import image_interpreter
+from src import image_reader
 
 
 def main():
-    """
-    * capture image
-    * extract image using image_interpreter
-    """
-    pass
+    captured_image = image_reader.read()
+    image_analyser = image_interpreter.ImageInterpreter(captured_image)
+    marks = image_analyser.extract()
+    print(marks)
 
 
 if __name__ == "__main__":
--- src/image_reader.py
+++ src/image_reader.py
@@ -1,12 +1,19 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
 
-    def read():
-        """
-        capture live answer script image from live feed
-        Use the below code for development and then use open cv to
-        """
+def read() -> np.ndarray:
+    """
+    capture live answer script image from live feed
+    Use the below code for development and then use open cv to
+    """
 
-        file_name = "original_img_1.jpg"
-        image = cv2.imread(f"test_images/original/{file_name}")
-        return image
+    file_name = "original_img_1.jpg"
+    image = cv2.imread(f"../test_images/original/{file_name}", cv2.COLOR_BGR2RGB)
+    return image
+
+
+if __name__ == "__main__":
+    plt.imshow(read())
+    plt.show()

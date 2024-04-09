893ecb676bef8bcf5839395c82bc1b46405f3ef7
don-sabu <donsabu@tutanota.com>
2024-04-09 16:11:44 +0530

    Added main function to table_extraction.py and updated requirements.txt

--- src/table_extraction.py
+++ src/table_extraction.py
@@ -1,18 +1,18 @@
 # This module will contain the code to segment each cell from the warped imaage.
 
 from img2table.document import Image
-from PIL import Image as PILImage
 import numpy as np
 import cv2
 
+
 def extract_table(img_path):
-    img = Image(src = img_path) 
+    img = Image(src=img_path)
     extracted_tables = img.extract_tables()
 
     return extracted_tables[0]
 
+
 def segment(image):
-    
     table_contents = extract_table(image).content
     img = cv2.imread(image)
     my_dict = {}
@@ -26,10 +26,10 @@ def segment(image):
                 new_img = img[cell.bbox.y1:cell.bbox.y2, cell.bbox.x1:cell.bbox.x2]
                 img_arr = np.array(new_img)
                 cell_list.append(img_arr)
-        cell_arr = np.array(cell_list, dtype = object)
-        reshaped_cell_list = cell_arr.reshape(3,13)
-        processed_cell_list = np.delete(reshaped_cell_list, 0, axis = 1)
-        
+        cell_arr = np.array(cell_list, dtype=object)
+        reshaped_cell_list = cell_arr.reshape(3, 13)
+        processed_cell_list = np.delete(reshaped_cell_list, 0, axis=1)
+
         for i in range(1, 13):
             key_list = []
             for sublist in processed_cell_list:
@@ -37,8 +37,10 @@ def segment(image):
             my_dict[str(i)] = key_list
 
         return my_dict
-    
+
     else:
         raise Exception("Table not detected!!!")
-        
-#segment('test_imgs\warpped\warpped')
+
+
+if __name__ == '__main__':
+    segment('test_imgs\warpped\warpped')

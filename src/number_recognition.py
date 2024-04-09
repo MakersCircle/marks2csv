886bed64239c1b516863bd6fabc190287df5052b
don-sabu <donsabu@tutanota.com>
2024-04-10 00:47:49 +0530

    added documentation to number_recognition.py

--- src/number_recognition.py
+++ src/number_recognition.py
@@ -1,5 +1,18 @@
 # this module is used to identify the numbers written in each cell using the model generated
-def recognise(cells) -> dict[int, list[int]]:
+import numpy as np
+
+
+def recognise(cells: dict[int, list[np.ndarray]]) -> dict[int, list[int]]:
+    """
+    Recognises the handwritten marks.
+    :param cells: Contain the image of each cell as dictionary where each key is the
+                  question number and corresponding value will be a list containing three
+                  ndarray representing images of three cells of the corresponding mark.
+    :return: Dictionary were key is the question number from 1 to 12 and value will be a list
+             containing marks of sub questions.
+    """
     raise NotImplemented("recognise() function is yet to be implemented")
 
-# this file is edited
+
+if __name__ == '__main__':
+    recognise()

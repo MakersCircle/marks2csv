e8d611e921263818f5e2e77088241c5ddf187bf0
Aneeta <itsaneetajose@gmail.com>
2024-04-10 17:46:23 +0530

    Updated table_extraction.py

--- src/table_extraction.py
+++ src/table_extraction.py
@@ -1,7 +1,6 @@
 # This module will contain the code to segment each cell from the warped image.
 
 from img2table.document import Image
-from PIL import Image as PILImage
 import numpy as np
 import cv2
 
@@ -9,7 +8,6 @@ import cv2
 def extract_table(img_path):
     img = Image(src=img_path)
     extracted_tables = img.extract_tables()
-    print(extracted_tables)
     return extracted_tables[0]
 
 
@@ -25,14 +23,12 @@ def segment(image):
 
         for row in table_contents.values():
             for cell in row:
-                cv2.rectangle(cp_table, (cell.bbox.x1, cell.bbox.y1), (cell.bbox.x2, cell.bbox.y2),(255, 0, 0), 3)
                 new_img = img[cell.bbox.y1:cell.bbox.y2, cell.bbox.x1:cell.bbox.x2]
                 img_arr = np.array(new_img)
                 cell_list.append(img_arr)
         cell_arr = np.array(cell_list, dtype=object)
         reshaped_cell_list = cell_arr.reshape(3, 13)
         processed_cell_list = np.delete(reshaped_cell_list, 0, axis=1)
-        PILImage.fromarray(cp_table)
 
         for i in range(1, 13):
             key_list = []
@@ -47,4 +43,4 @@ def segment(image):
 
 
 if __name__ == '__main__':
-    segment('test_images\warpped\warpped.jpg')
+    print(segment('../test_images\warpped\warpped1.jpg'))

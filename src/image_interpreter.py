4cc1167d72283675334c5ca91ce4920b7f143060
Jacob Philip <jacobauxilium@gmail.com>
2024-04-30 17:12:13 +0530

    Modified image_interpreter.py to match formats of app.py

--- src/image_interpreter.py
+++ src/image_interpreter.py
@@ -1,47 +1,34 @@
 from . import warping
 from . import table_extraction
 from . import number_recognition
-
-import numpy as np
 import pandas as pd
-
-
 class ImageInterpreter:
     def __init__(self, image):
-        self.marks: dict[int, list[float]] = {}
-        self.image: np.ndarray = image
+        self.marks: dict[int, list[tuple[float, float]]] = {}
+        self.image = image
         self.results = {}
 
     def format_result(self) -> None:
-
         for key, values in self.results.items():
             self.marks[key] = []
-            for mark_str, _ in values:
+            for mark_str, confidence in values:
                 try:
-                    mark = float(mark_str)
-                    if mark.is_integer():
-                        mark = int(mark)
+                    mark = round(float(mark_str),1)
                 except ValueError:
-                    mark = 0
-                self.marks[key].append(mark)
-
+                    mark = 0.0
+                try:
+                    confidence = round(float(confidence),1)
+                except ValueError:
+                    confidence = 0.0
+                self.marks[key].append((mark, confidence))
     def extract(self) -> None:
-        """
-        A pipeline for the processes:
-        i) Warp the captured image.
-        ii) Extract each individual cells from the table containing the handwritten marks.
-        iii) Detect handwritten digit of each cell.
-        """
-
         warped_image = warping.warp(self.image)
         cells = table_extraction.segment(warped_image)
         self.results = number_recognition.recognise(cells)
         self.format_result()
 
     def __str__(self):
-        """
-        Display the mark of a single answer sheet
-        """
+        # String representation of the marks in a DataFrame format
         if not self.marks:
             return "No Marks Detected."
         else:
